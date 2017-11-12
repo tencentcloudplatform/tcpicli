0f0cd53423bd8ade19d030b774b88bf14b483a91
james oppel <jamesoppel@tencent.com>
2017-11-12 22:23:57 +0800

    finished ccs code. need to clean up autogen stuff, but entire ccs suite is functional now

--- ccs/gen.go
+++ ccs/gen.go
@@ -17,20 +17,23 @@ func (p *Pkg) DoAction(action string, query ...string) ([]byte, error) {
 
 func main() {
 	region := "echo gz"
-	vpcId := "echo vpc-l89napt5"       // need hardcode work
-	subnetId := "echo subnet-at7hi4d6" // need hardcode work
+	vpcId := "echo vpc-l89napt5"       // need hardcode work i.e. make intermediate key and then delete upon completion of autogenerate suitek
+	subnetId := "echo subnet-at7hi4d6" // need hardcode work i.e. make intermediate key and then delete upon completion of autogenerate suitek
 	clusterID := "apitest"
 	bandwidthType := "echo PayByTraffic"
 	bandwidth := "echo 100"
-	keyId := "echo skey-impvf5cj" // need hardcode work
+	keyId := "echo skey-impvf5cj" // need hardcode work i.e. make intermediate key and then delete upon completion of autogenerate suite
 	clusterCIDR := "echo 172.16.0.0/16"
 	isVpcGateway := "echo 0"
 	zoneId := "echo 100002"
-	serviceName := "echo apitestnginx"
+	serviceName := "echo apitest"
+	serviceDescription := "echo descriptionnowhitespace"
 	osName := "echo centos7.2x86_64"
 	nginxImage := "echo nginx:latest"
 	nginxPort := "echo 80"
 	nginxProto := "echo TCP"
+	replicasNew := "echo 1"
+	replicasMod := "echo 5"
 	cpu := "echo 2"
 	mem := "echo 4"
 	storageSize := "echo 50"
@@ -41,7 +44,7 @@ func main() {
 		DocRoot: "https://cloud.tencent.com/document/api/",
 		Seq: []string{
 			`SET clusterID=tcpicli -f "{{range .Data.Clusters}}{{.ClusterID}}{{end}}" ccs DescribeCluster Region=gz clusterName=` + clusterID,
-			`SET serviceName=tcpicli -f ""`,
+			"SET serviceName=" + serviceName,
 			"SET Region=" + region,
 			"SET vpcId=" + vpcId,
 			"SET keyId=" + keyId,
@@ -58,20 +61,33 @@ func main() {
 			"SET storageSize=" + storageSize,
 			"SET rootSize=" + rootSize,
 			"SET goodsNum=" + goodsNum,
+			"SET replicasNew=" + replicasNew,
+			"SET replicasMod=" + replicasMod,
 			"SET nginxImage=" + nginxImage,
 			"SET nginxPort=" + nginxPort,
 			"SET nginxProto=" + nginxProto,
 			"SET nodeDeleteMode=" + nodeDeleteMode,
+			"SET serviceDescription=" + serviceDescription,
+			`SET deleteInstances=tcpicli -f "{{range .Data.Instances}}{{.Name}}{{\" \"}}{{end}}" ccs DescribeServiceInstance Region=$Region clusterId=$clusterID serviceName=$serviceName | awk '{ print $1 }'`,
 			// "CreateCluster",
 			// "AddClusterInstances",
 			// "AddClusterInstancesFromExistedCvm",
 			// "DescribeCluster",
 			// "DescribeClusterInstances",
 			// "DeleteClusterInstances", // need hardcode work
-			// "DeleteCluster",
+			// "DeleteCluster", // defer
 			// "CreateClusterService",
 			// "DescribeClusterService",
-			"DescribeClusterServiceInfo",
+			// "DescribeClusterServiceInfo",
+			// "ModifyClusterService",
+			// "PauseClusterService",
+			// "ResumeClusterService",
+			// "ModifyServiceDescription",
+			// "DescribeServiceEvent",
+			// "RollBackClusterService",
+			// "DescribeServiceInstance",
+			// "ModifyServiceReplicas",
+			"DeleteInstances",
 			// "DeleteClusterService",
 		},
 		FuncMap: map[string][]string{
@@ -128,7 +144,7 @@ func main() {
 				"Region=$Region",
 				"clusterId=$clusterID",
 				"serviceName=$serviceName",
-				"replicas=1",
+				"replicas=$replicasMod",
 				"containers.0.image=$nginxImage",
 				"containers.0.containerName=$serviceName",
 				"portMappings.0.containerPort=$nginxPort",
@@ -141,7 +157,64 @@ func main() {
 				"serviceName=$serviceName",
 			},
 			"DescribeClusterServiceInfo": []string{"457/9441",
+				"Region=$Region",
 				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+			},
+			"ModifyClusterService": []string{"457/9434",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+				"replicas=$replicasMod",
+				"containers.0.image=$nginxImage",
+				"containers.0.containerName=$serviceName",
+				"portMappings.0.containerPort=$nginxPort",
+				"portMappings.0.lbPort=$nginxPort",
+				"portMappings.0.protocol=$nginxProto",
+				"strategy=RollingUpdate",
+				"minReadySeconds=10",
+			},
+			"PauseClusterService": []string{"457/9439",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+			},
+			"ResumeClusterService": []string{"457/9442",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+			},
+			"ModifyServiceDescription": []string{"457/9434",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+				"serviceDescription=$serviceDescription",
+			},
+			"DescribeServiceEvent": []string{"457/9443",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+			},
+			"RollBackClusterService": []string{"457/9438",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+			},
+			"DescribeServiceInstance": []string{"457/9433",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+			},
+			"ModifyServiceReplicas": []string{"457/9431",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"serviceName=$serviceName",
+				"scaleTo=$replicasMod",
+			},
+			"DeleteInstances": []string{"457/9432",
+				"Region=$Region",
+				"clusterId=$clusterID",
+				"instances.0=$deleteInstances",
 			},
 			"DeleteClusterService": []string{"457/9437",
 				"Region=$Region",
--- tcpicli/ccs.go
+++ tcpicli/ccs.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	// "encoding/json"
-	// "encoding/json"
 	"fmt"
 	"github.com/tencentcloudplatform/tcpicli/ccs"
 	"github.com/urfave/cli"
@@ -51,6 +49,84 @@ var (
 			Usage:       "Removes a CVM node from a CCS cluster. If CVM is postpaid, it can be terminated after removal.",
 			Action:      CcsDeleteClusterInstances,
 			Description: "referer: https://cloud.tencent.com/document/api/457/9446",
+		},
+		{
+			Name:        "CreateClusterService",
+			Usage:       "Creates a new service on a cluster.",
+			Action:      CcsCreateClusterService,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9436",
+		},
+		{
+			Name:        "DescribeClusterService",
+			Usage:       "Describes service.",
+			Action:      CcsDescribeClusterService,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9440",
+		},
+		{
+			Name:        "DescribeClusterServiceInfo",
+			Usage:       "Details particular service.",
+			Action:      CcsDescribeClusterServiceInfo,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9441",
+		},
+		{
+			Name:        "DescribeServiceEvent",
+			Usage:       "Details particular service events in last hour.",
+			Action:      CcsDescribeServiceEvent,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9443",
+		},
+		{
+			Name:        "RollBackClusterService",
+			Usage:       "Roll-back service modification. Only supports one iteration",
+			Action:      CcsRollBackClusterService,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9438",
+		},
+		{
+			Name:        "DescribeServiceInstance",
+			Usage:       "Lists nodes that are used in a particular service",
+			Action:      CcsDescribeServiceInstance,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9433",
+		},
+		{
+			Name:        "ModifyServiceReplicas",
+			Usage:       "Resizes pod replicas in service",
+			Action:      CcsModifyServiceReplicas,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9431",
+		},
+		{
+			Name:        "DeleteInstances",
+			Usage:       "Deletes pods from service",
+			Action:      CcsDeleteInstances,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9432",
+		},
+		{
+			Name:        "ModifyClusterService",
+			Usage:       "Modifies service on a cluster.",
+			Action:      CcsModifyClusterService,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9434",
+		},
+		{
+			Name:        "PauseClusterService",
+			Usage:       "Pauses modification on service.",
+			Action:      CcsPauseClusterService,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9439",
+		},
+		{
+			Name:        "ResumeClusterService",
+			Usage:       "Resumes paused modification.",
+			Action:      CcsResumeClusterService,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9442",
+		},
+		{
+			Name:        "ModifyServiceDescription", // just add a description to the service. No need to complicate code by changing serviceName or anything like that
+			Usage:       "Modifies descriptive data on a service.",
+			Action:      CcsModifyServiceDescription,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9435",
+		},
+		{
+			Name:        "DeleteClusterService",
+			Usage:       "Details particular service.",
+			Action:      CcsDeleteClusterService,
+			Description: "referer: https://cloud.tencent.com/document/api/457/9437",
 		},
 	}
 )
@@ -126,6 +202,162 @@ func CcsDescribeClusterInstances(c *cli.Context) error {
 }
 func CcsDeleteClusterInstances(c *cli.Context) error {
 	resp, err := ccs.DeleteClusterInstances(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsCreateClusterService(c *cli.Context) error {
+	resp, err := ccs.CreateClusterService(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsDescribeClusterService(c *cli.Context) error {
+	resp, err := ccs.DescribeClusterService(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsDescribeClusterServiceInfo(c *cli.Context) error {
+	resp, err := ccs.DescribeClusterServiceInfo(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsModifyClusterService(c *cli.Context) error {
+	resp, err := ccs.ModifyClusterService(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsPauseClusterService(c *cli.Context) error {
+	resp, err := ccs.PauseClusterService(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsResumeClusterService(c *cli.Context) error {
+	resp, err := ccs.ResumeClusterService(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsModifyServiceDescription(c *cli.Context) error {
+	resp, err := ccs.ModifyServiceDescription(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsDescribeServiceEvent(c *cli.Context) error {
+	resp, err := ccs.DescribeServiceEvent(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsRollBackClusterService(c *cli.Context) error {
+	resp, err := ccs.RollBackClusterService(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsDescribeServiceInstance(c *cli.Context) error {
+	resp, err := ccs.DescribeServiceInstance(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsModifyServiceReplicas(c *cli.Context) error {
+	resp, err := ccs.ModifyServiceReplicas(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsDeleteInstances(c *cli.Context) error {
+	resp, err := ccs.DeleteInstances(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func CcsDeleteClusterService(c *cli.Context) error {
+	resp, err := ccs.DeleteClusterService(c.Args()...)
 	if err != nil {
 		return err
 	}
