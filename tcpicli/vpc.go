310fc452930f1eed382e7416b97df59116c215fe
james oppel <jamesoppel@tencent.com>
2017-11-13 21:54:19 -0800

    more vpc features

--- tcpicli/vpc.go
+++ tcpicli/vpc.go
@@ -19,6 +19,24 @@ var (
 			Action:      VpcCreateVpc,
 			Description: "Referrer: https://cloud.tencent.com/document/215/1309",
 		},
+		{
+			Name:        "DescribeVpcEx",
+			Usage:       "Queries VPC list.",
+			Action:      VpcDescribeVpcEx,
+			Description: "Referrer: https://cloud.tencent.com/document/215/1372",
+		},
+		{
+			Name:        "DescribeVpcClassicLink",
+			Usage:       "Describes classic link between VPC and CVM",
+			Action:      VpcDescribeVpcClassicLink,
+			Description: "Referrer: https://cloud.tencent.com/document/215/1844",
+		},
+		{
+			Name:        "DescribeVpcLimit",
+			Usage:       "Describes VPC limits.",
+			Action:      VpcDescribeVpcLimit,
+			Description: "Referrer: https://cloud.tencent.com/document/215/2112",
+		},
 		//{
 		//	Name:        "action",
 		//	Usage:       "",
@@ -38,6 +56,42 @@ func VpcDoAction(c *cli.Context) error {
 }
 func VpcCreateVpc(c *cli.Context) error {
 	resp, err := vpc.CreateVpc(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeVpcEx(c *cli.Context) error {
+	resp, err := vpc.DescribeVpcEx(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeVpcClassicLink(c *cli.Context) error {
+	resp, err := vpc.DescribeVpcClassicLink(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeVpcLimit(c *cli.Context) error {
+	resp, err := vpc.DescribeVpcLimit(c.Args()...)
 	if err != nil {
 		return err
 	}
--- vpc/gen.go
+++ vpc/gen.go
@@ -16,25 +16,34 @@ func (p *Pkg) DoAction(action string, query ...string) ([]byte, error) {
 func main() {
 	Region := "echo gz"
 	availabilityZone := "echo 100002"
-	vpcName := "echo tcpiclivpc"
+	vpcName := "echo initialtcpiclivpc"
+	vpcNameMod := "echo tcpiclivpc"
 	cidrBlock := "echo 192.168.0.0/16"
 	subnetCidr0 := "echo 192.168.0.0/24"
 	subnetCidr1 := "echo 192.168.1.0/24"
 	subnetName0 := "echo tcpclisub0"
 	subnetName1 := "echo tcpclisub1"
+	vpcType := "echo 1"
 
 	gen := &autogen.Gen{
 		DocRoot: "https://cloud.tencent.com/document/api/",
 		Seq: []string{
 			"SET Region=" + Region,
 			"SET vpcName=" + vpcName,
+			"Set vpcNameMod" + vpcNameMod,
 			"SET cidrBlock=" + cidrBlock,
 			"SET subnetCidr0=" + subnetCidr0,
 			"SET subnetCidr1=" + subnetCidr1,
 			"SET subnetName0=" + subnetName0,
 			"SET subnetName1=" + subnetName1,
 			"SET zoneId=" + availabilityZone,
-			"CreateVpc",
+			"SET vpcType=" + vpcType,
+			// "CreateVpc",
+			`SET vpcId=tcpicli -f `,
+			// "DescribeVpcEx",
+			// "DescribeVpcClassicLink",
+			// "DescribeVpcLimit",
+			"ModifyVpcAttribute",
 		},
 		FuncMap: map[string][]string{
 			"CreateVpc": []string{"215/1309",
@@ -47,6 +56,19 @@ func main() {
 				"subnetSet.1.subnetName=$subnetName1",
 				"subnetSet.0.zoneId=$zoneId",
 				"subnetSet.1.zoneId=$zoneId",
+			},
+			"DescribeVpcEx": []string{"215/1372",
+				"Region=$Region",
+			},
+			"DescribeVpcClassicLink": []string{"215/2112",
+				"Region=$Region",
+			},
+			"DescribeVpcLimit": []string{"215/1844",
+				"Region=$Region",
+				"type.0=$vpcType",
+			},
+			"ModifyVpcAttribute": []string{"215/1310",
+				"Region=$Region",
 			},
 		},
 		PkgName: "vpc",
