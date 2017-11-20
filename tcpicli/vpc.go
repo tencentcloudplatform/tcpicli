6b4861e87b531bfa88d41ba8281a5369e87b9b31
james oppel <jamesoppel@tencent.com>
2017-11-20 11:56:28 -0800

    more VPC funcs

--- tcpicli/vpc.go
+++ tcpicli/vpc.go
@@ -17,49 +17,157 @@ var (
 			Name:        "CreateVpc",
 			Usage:       "Creates a new VPC.",
 			Action:      VpcCreateVpc,
-			Description: "Referrer: https://cloud.tencent.com/document/215/1309",
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1309",
 		},
 		{
 			Name:        "DescribeVpcEx",
 			Usage:       "Queries VPC list.",
 			Action:      VpcDescribeVpcEx,
-			Description: "Referrer: https://cloud.tencent.com/document/215/1372",
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1372",
 		},
 		{
 			Name:        "DescribeVpcClassicLink",
 			Usage:       "Describes classic link between VPC and CVM",
 			Action:      VpcDescribeVpcClassicLink,
-			Description: "Referrer: https://cloud.tencent.com/document/215/1844",
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1844",
 		},
 		{
 			Name:        "DescribeVpcLimit",
 			Usage:       "Describes VPC limits.",
 			Action:      VpcDescribeVpcLimit,
-			Description: "Referrer: https://cloud.tencent.com/document/215/2112",
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/2112",
 		},
 		{
 			Name:        "ModifyVpcAttribute",
 			Usage:       "Modifies VPC attribute",
 			Action:      VpcModifyVpcAttribute,
-			Description: "Referrer: https://cloud.tencent.com/document/215/1310",
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1310",
 		},
 		// {
 		// 	Name:        "AssociateVip",
 		// 	Usage:       "Binds EIP to instance",
 		// 	Action:      VpcAssociateVip,
-		// 	Description: "Referrer: https://cloud.tencent.com/document/215/1361",
+		// 	Description: "Referrer: https://cloud.tencent.com/document/api/215/1361",
 		// },
 		//{
 		//	Name:        "AttachClassicLinkVpc",
 		//	Usage:       "Binds a CVM to a VPC.",
 		//	Action:      VpcAttachClassicLinkVpc,
-		//	Description: "Referrer: https://cloud.tencent.com/document/215/2098",
+		//	Description: "Referrer: https://cloud.tencent.com/document/api/215/2098",
 		//},
 		{
 			Name:        "DeleteVpc",
 			Usage:       "Deletes a VPC. Can't have any resources in VPC.",
 			Action:      VpcDeleteVpc,
-			Description: "Referrer: https://cloud.tencent.com/document/215/1307",
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1307",
+		},
+		{
+			Name:        "CreateSubnet",
+			Usage:       "Creates subnets in existing VPCs",
+			Action:      VpcCreateSubnet,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1314",
+		},
+		{
+			Name:        "DescribeSubnetEx",
+			Usage:       "Queries subnet list",
+			Action:      VpcDescribeSubnetEx,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1371",
+		},
+		{
+			Name:        "DescribeSubnet",
+			Usage:       "Gives detailed infromation about particular subnet",
+			Action:      VpcDescribeSubnet,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1311",
+		},
+		{
+			Name:        "DeleteSubnet",
+			Usage:       "Deletes subnet.",
+			Action:      VpcDeleteSubnet,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1312",
+		},
+		{
+			Name:        "CreateRouteTable",
+			Usage:       "Creates a new route table",
+			Action:      VpcCreateRouteTable,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1419",
+		},
+		{
+			Name:        "AssociateRouteTable",
+			Usage:       "Associates a route table",
+			Action:      VpcAssociateRouteTable,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1416",
+		},
+		{
+			Name:        "CreateNetworkAcl",
+			Usage:       "Creates a new NACL.",
+			Action:      VpcCreateNetworkAcl,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1437",
+		},
+		{
+			Name:        "DescribeNetworkAcl",
+			Usage:       "Queries NACL list",
+			Action:      VpcDescribeNetworkAcl,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1441",
+		},
+		{
+			Name:        "ModifyNetworkAcl",
+			Usage:       "Changes the name of NACL",
+			Action:      VpcModifyNetworkAcl,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1443",
+		},
+		{
+			Name:        "DeleteNetworkAcl",
+			Usage:       "Deletes existing NACL.",
+			Action:      VpcDeleteNetworkAcl,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1439",
+		},
+		{
+			Name:        "CreateVpcPeeringConnection",
+			Usage:       "Creates a VPC peering connection",
+			Action:      VpcCreateVpcPeeringConnection,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/2107",
+		},
+		{
+			Name:        "DescribeVpcPeeringConnections",
+			Usage:       "Lists all existing VPC peering connections",
+			Action:      VpcDescribeVpcPeeringConnections,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/2101",
+		},
+		{
+			Name:        "DeleteVpcPeeringConnection",
+			Usage:       "Deletes a VPC peering connection.",
+			Action:      VpcDeleteVpcPeeringConnection,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/2104",
+		},
+		{
+			Name:        "DescribeRouteTable",
+			Usage:       "Describes RTs.",
+			Action:      VpcDescribeRouteTable,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1420",
+		},
+		{
+			Name:        "CreateRoute",
+			Usage:       "Creates a route in specified RT",
+			Action:      VpcCreateRoute,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/5688",
+		},
+		{
+			Name:        "DeleteRoute",
+			Usage:       "Deletes a route in specified RT",
+			Action:      VpcDeleteRoute,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/5689",
+		},
+		{
+			Name:        "DeleteRouteTable",
+			Usage:       "Deletes RT.",
+			Action:      VpcDeleteRouteTable,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1418",
+		},
+		{
+			Name:        "ModifyRouteTableAttribute",
+			Usage:       "Modifies RT attribute.",
+			Action:      VpcModifyRouteTableAttribute,
+			Description: "Referrer: https://cloud.tencent.com/document/api/215/1417",
 		},
 		//{
 		//	Name:        "action",
@@ -151,7 +259,6 @@ func VpcModifyVpcAttribute(c *cli.Context) error {
 // 	fmt.Println(r)
 // 	return nil
 // }
-
 //func VpcAttachClassicLinkVpc(c *cli.Context) error {
 //	resp, err := vpc.AttachClassicLinkVpc(c.Args()...)
 //	if err != nil {
@@ -166,6 +273,222 @@ func VpcModifyVpcAttribute(c *cli.Context) error {
 //}
 func VpcDeleteVpc(c *cli.Context) error {
 	resp, err := vpc.DeleteVpc(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcCreateSubnet(c *cli.Context) error {
+	resp, err := vpc.CreateSubnet(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeSubnetEx(c *cli.Context) error {
+	resp, err := vpc.DescribeSubnetEx(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeSubnet(c *cli.Context) error {
+	resp, err := vpc.DescribeSubnet(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDeleteSubnet(c *cli.Context) error {
+	resp, err := vpc.DeleteSubnet(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcCreateRouteTable(c *cli.Context) error {
+	resp, err := vpc.CreateRouteTable(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcAssociateRouteTable(c *cli.Context) error {
+	resp, err := vpc.AssociateRouteTable(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcCreateVpcPeeringConnection(c *cli.Context) error {
+	resp, err := vpc.CreateVpcPeeringConnection(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeVpcPeeringConnections(c *cli.Context) error {
+	resp, err := vpc.DescribeVpcPeeringConnections(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDeleteVpcPeeringConnection(c *cli.Context) error {
+	resp, err := vpc.DeleteVpcPeeringConnection(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeRouteTable(c *cli.Context) error {
+	resp, err := vpc.DescribeRouteTable(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDeleteRouteTable(c *cli.Context) error {
+	resp, err := vpc.DeleteRouteTable(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcCreateRoute(c *cli.Context) error {
+	resp, err := vpc.CreateRoute(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDeleteRoute(c *cli.Context) error {
+	resp, err := vpc.DeleteRoute(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcModifyRouteTableAttribute(c *cli.Context) error {
+	resp, err := vpc.ModifyRouteTableAttribute(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcCreateNetworkAcl(c *cli.Context) error {
+	resp, err := vpc.CreateNetworkAcl(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDescribeNetworkAcl(c *cli.Context) error {
+	resp, err := vpc.DescribeNetworkAcl(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcDeleteNetworkAcl(c *cli.Context) error {
+	resp, err := vpc.DeleteNetworkAcl(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VpcModifyNetworkAcl(c *cli.Context) error {
+	resp, err := vpc.ModifyNetworkAcl(c.Args()...)
 	if err != nil {
 		return err
 	}
--- vpc/gen.go
+++ vpc/gen.go
@@ -14,89 +14,207 @@ func (p *Pkg) DoAction(action string, query ...string) ([]byte, error) {
 }
 
 func main() {
-	Region := "echo gz"
-	availabilityZone := "echo 100002"
-	vpcName := "echo initialtcpiclivpc"
-	vpcNameMod := "echo tcpiclivpc"
-	vpcId := "echo vpc-ncetpc2r"           // hardcode
-	vpcTestInstance := "echo ins-kjhjjaki" // hardcode
-	cidrBlock := "echo 10.0.0.0/16"
-	subnetCidr0 := "echo 10.0.0.0/24"
-	subnetCidr1 := "echo 10.0.1.0/24"
-	subnetName0 := "echo tcpclisub0"
-	subnetName1 := "echo tcpclisub1"
-	vpcType := "echo 1"
-	vipId := "echo eip-kw5x9v3i" // hardcode
-	lanIp := "echo "             // hardcode need fix
+	peerUin := "peerUin=100000655192"
+	region := "Region=gz"
+	zoneId := "subnetSet.0.zoneId=100002"
+	vpcNameInit := "vpcName=tcpiclivpcinit"
+	vpcName0 := "vpcName=tcpiclivpc"
+	vpcName1 := "vpcName=pcxonly"
+	cidrBlock0 := "cidrBlock=10.0.0.0/16"
+	cidrBlock1 := "cidrBlock=192.168.0.0/16"
+	subnetCidr0 := "subnetSet.0.cidrBlock=10.0.0.0/24"
+	subnetCidr1 := "subnetSet.0.cidrBlock=192.168.0.0/24"
+	subnetName0 := "subnetSet.0.subnetName=tcpclisub0"
+	subnetName1 := "subnetSet.0.subnetName=tcpclisub1"
+	routeTableName := "routeTableName=tcpiclirt"
+	destinationCidrBlock0 := subnetCidr1
+	nextHub0 := ""
+	nextType0 := "4"
+	peeringConnectionName := "peeringConnectionName=tcpiclipcx"
+	// -- for adding and deleting routes in Subnet API actions --
+	destinationCidrBlockAdd := "routeSet.0.destinationCidrBlock=172.217.6.46/32"
+	nextTypeAdd := "routeSet.0.nextType=" + nextType0
+	// -- for adding and deleting NACL stuff --
+	networkAclName := "networkAclName=tcpiclinacl"
+	networkAclNameMod := networkAclName + "new"
+
 	gen := &autogen.Gen{
 		DocRoot: "https://cloud.tencent.com/document/api/",
 		Seq: []string{
-			"SET Region=" + Region,
-			"SET vpcName=" + vpcName,
-			"SET cidrBlock=" + cidrBlock,
-			"SET subnetCidr0=" + subnetCidr0,
-			"SET subnetCidr1=" + subnetCidr1,
-			"SET subnetName0=" + subnetName0,
-			"SET subnetName1=" + subnetName1,
-			"SET zoneId=" + availabilityZone,
-			"SET vpcType=" + vpcType,
-			// "CreateVpc",
-			// `SET vpcId=tcpicli -f "{{range .Data}}{{.UnVpcID}}{{end}}" vpc DescribeVpcEx Region=$Region vpcName=$vpcName`, // need to fix vpc package
-			// "DescribeVpcEx",
-			// "DescribeVpcClassicLink",
-			// "DescribeVpcLimit",
-			"SET vpcNameMod=" + vpcNameMod,
-			"SET vpcId=" + vpcId, // hardcode
-			// "ModifyVpcAttribute",
-			// `SET vpcId=tcpicli -f "{{range .Data}}{{.UnVpcID}}{{end}}" vpc DescribeVpcEx Region=$Region vpcName=$vpcNameMod`, // need to fix vpc package
-			"SET vipId=" + vipId, // hardcode
-			"SET lanIp=" + lanIp, // hardcode
+			// -- don't work or need to engage vpc team to figure out api --
 			// "AssociateVip",
-			"SET instanceIds.0=" + vpcTestInstance,
 			// "AttachClassicLinkVpc",
+			// "DescribeVpcLimit",
+
+			"CreateVpc",
+			"DO tcpicli vpc CreateVpc " + region + " " + vpcName1 + " " + cidrBlock1,
+			`SET vpcId0=tcpicli -f "{{range .Data}}{{.UnVpcID}}{{end}}" vpc DescribeVpcEx ` + region + " " + vpcName0,
+			"DescribeVpcEx",
+			`SET vpcId1=tcpicli -f "{{range .Data}}{{.UnVpcID}}{{end}}" vpc DescribeVpcEx ` + region + " " + vpcName1,
+			"DescribeVpcClassicLink",
+			"ModifyVpcAttribute",
+			"CreateSubnet",
+			"DO tcpicli vpc CreateSubnet vpcId=$vpcId1 " + region + " " + subnetCidr1 + " " + subnetName1 + " " + zoneId,
+			"DescribeSubnetEx",
+			`SET subnetId0=tcpicli -f "{{range .Data}}{{.UnSubnetID}}{{end}}" vpc DescribeSubnetEx vpcId=$vpcId0 ` + region,
+			"DescribeSubnet",
+			`SET subnetId1=tcpicli -f "{{range .Data}}{{.UnSubnetID}}{{end}}" vpc DescribeSubnetEx vpcId=$vpcId1 ` + region,
+			"CreateVpcPeeringConnection",
+			"DescribeVpcPeeringConnections",
+			`SET peeringConnectionId=tcpicli -f "{{range .Data}}{{.peeringConnectionId}}{{end}}" vpc DescribeVpcPeeringConnections ` + region + " " + peeringConnectionName,
+			"CreateRouteTable",
+			"DescribeRouteTable",
+			`SET routeTableId=tcpicli -f "{{range .Data}}{{.UnRouteTableID}}{{end}}" vpc DescribeRouteTable vpcId=$vpcId0 ` + region + " " + routeTableName,
+			"ModifyRouteTableAttribute",
+			"CreateRoute",
+			"AssociateRouteTable",
+			"CreateNetworkAcl",
+			"DescribeNetworkAcl",
+			`SET networkAclId=tcpicli -f "{{range .Data}}{{.NetworkAclID}}{{end}}" vpc DescribeNetworkAcl vpcId=$vpcId0 ` + region + " " + networkAclName,
+			"ModifyNetworkAcl",
+			// -- clean everything up --
+			"DeleteNetworkAcl",
+			"DeleteRoute",
+			// -- RT doesn't delete if bound to subnet. No API action to dissociate RT from subnet. RT gets deleted when subnet it is bound to gets deleted. --
+			"DeleteRouteTable",
+			"DeleteVpcPeeringConnection",
+			// -- Bound RT gets deleted here. Failure on 'DeleteRouteTable' expected. If regen to 'DeleteRouteTable.go' needed, comment out 'AssociateRouteTable' above  --
+			"DeleteSubnet",
 			"DeleteVpc",
+			// -- clean up manual stuff --
+			"DO tcpicli vpc DeleteSubnet vpcId=$vpcId1 subnetId=$subnetId1 " + region,
+			"DO tcpicli vpc DeleteVpc vpcId=$vpcId1 " + region,
 		},
 		FuncMap: map[string][]string{
 			"CreateVpc": []string{"215/1309",
-				"Region=$Region",
-				"vpcName=$vpcName",
-				"cidrBlock=$cidrBlock",
-				"subnetSet.0.cidrBlock=$subnetCidr0",
-				"subnetSet.1.cidrBlock=$subnetCidr1",
-				"subnetSet.0.subnetName=$subnetName0",
-				"subnetSet.1.subnetName=$subnetName1",
-				"subnetSet.0.zoneId=$zoneId",
-				"subnetSet.1.zoneId=$zoneId",
+				region,
+				vpcNameInit,
+				cidrBlock0,
 			},
 			"DescribeVpcEx": []string{"215/1372",
-				"Region=$Region",
+				region,
 			},
 			"DescribeVpcClassicLink": []string{"215/2112",
-				"Region=$Region",
+				region,
 			},
 			"DescribeVpcLimit": []string{"215/1844",
-				"Region=$Region",
-				"type.0=$vpcType",
+				region,
 			},
 			"ModifyVpcAttribute": []string{"215/1310",
-				"Region=$Region",
-				"vpcId=$vpcId",
-				"vpcName=$vpcNameMod",
+				region,
+				"vpcId=$vpcId0",
+				vpcName0,
 			},
 			"AssociateVip": []string{"215/1361",
-				"Region=$Region",
-				"vpcId=$vpcId",
-				"vipId=$vipId",
-				"lanIp=$lanIp",
+				region,
+				"vpcId=$vpcId0",
 			},
 			"AttachClassicLinkVpc": []string{"215/2098",
-				"Region=$Region",
-				"vpcId=$vpcId",
-				"instanceIds.0=$vpcTestInstance",
+				region,
+				"vpcId=$vpcId0",
 			},
 			"DeleteVpc": []string{"215/1307",
-				"Region=$Region",
-				"vpcId=$vpcId",
+				region,
+				"vpcId=$vpcId0",
+			},
+			"CreateSubnet": []string{"215/1314",
+				region,
+				"vpcId=$vpcId0",
+				subnetCidr0,
+				subnetName0,
+				zoneId,
+			},
+			"DescribeSubnetEx": []string{"215/1371",
+				region,
+				"vpcId=$vpcId0",
+			},
+			"DescribeSubnet": []string{"215/1311",
+				region,
+				"vpcId=$vpcId0",
+				"subnetId=$subnetId0",
+			},
+			"CreateVpcPeeringConnection": []string{"215/2107",
+				region,
+				peeringConnectionName,
+				"vpcId=$vpcId0",
+				"peerVpcId=$vpcId1",
+				peerUin,
+			},
+			"DescribeVpcPeeringConnections": []string{"215/2101"},
+			"DeleteVpcPeeringConnection": []string{"215/2104",
+				region,
+				"peeringConnectionId=$peeringConnectionId",
+			},
+			"CreateRouteTable": []string{"215/1419",
+				region,
+				"vpcId=$vpcId0",
+				"subnetId=$subnetId0",
+				routeTableName,
+				destinationCidrBlock0,
+				nextType0,
+				nextHub0,
+			},
+			"DescribeRouteTable": []string{"215/1420",
+				region,
+				"vpcId=$vpcId0",
+				routeTableName,
+			},
+			"ModifyRouteTableAttribute": []string{"215/1417",
+				region,
+				"vpcId=$vpcId0",
+				"routeTableId=$routeTableId",
+			},
+			"CreateRoute": []string{"215/5688",
+				region,
+				destinationCidrBlockAdd,
+				nextTypeAdd,
+				"routeSet.0.nextHub=$peeringConnectionId",
+				"vpcId=$vpcId0",
+				"routeTableId=$routeTableId",
+			},
+			"AssociateRouteTable": []string{"215/1416",
+				region,
+				"vpcId=$vpcId0",
+				"routeTableId=$routeTableId",
+				"subnetId=$subnetId0",
+			},
+			"CreateNetworkAcl": []string{"215/1437",
+				region,
+				networkAclName,
+				"vpcId=$vpcId0",
+			},
+			"DescribeNetworkAcl": []string{"215/1441",
+				region,
+				"vpcId=$vpcId0",
+			},
+			"ModifyNetworkAcl": []string{"215/1443",
+				region,
+				networkAclNameMod,
+				"vpcId=$vpcId0",
+				"networkAclId=$networkAclId",
+			},
+			"DeleteNetworkAcl": []string{"215/1439",
+				region,
+				"vpcId=$vpcId0",
+				"networkAclId=$networkAclId",
+			},
+			"DeleteRoute": []string{"215/5689",
+				region,
+				destinationCidrBlockAdd,
+				nextTypeAdd,
+				"routeSet.0.nextHub=$peeringConnectionId",
+				"vpcId=$vpcId0",
+				"routeTableId=$routeTableId",
+			},
+			"DeleteRouteTable": []string{"215/1418",
+				region,
+				"vpcId=$vpcId0",
+				"routeTableId=$routeTableId",
+			},
+			"DeleteSubnet": []string{"215/1312",
+				region,
+				"vpcId=$vpcId0",
+				"subnetId=$subnetId0",
 			},
 		},
 		PkgName: "vpc",
