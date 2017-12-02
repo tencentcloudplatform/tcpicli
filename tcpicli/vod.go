18107c1dbdae232d6d94753b709def15ff262c4b
James Oppel <jamesoppel@tencent.com>
2017-12-01 17:32:45 -0800

    almost done with vod

--- tcpicli/vod.go
+++ tcpicli/vod.go
@@ -14,46 +14,124 @@ var (
 			Action: VodDoAction,
 		},
 		{
-			Name:        "ApplyUpload",
-			Usage:       "ApplyUpload",
-			Action:      VodApplyUpload,
-			Description: "Referer: https://cloud.tencent.com/document/api/266/7788",
-		},
-		{
-			Name:        "DescribeVodPlayInfo",
-			Usage:       "DescribeVodPlayInfo",
-			Action:      VodDescribeVodPlayInfo,
-			Description: "Referer: https://cloud.tencent.com/document/api/266/7825",
-		},
-		{
-			Name:        "GetVideoInfo",
-			Usage:       "GetVideoInfo",
-			Action:      VodGetVideoInfo,
-			Description: "Referer: https://cloud.tencent.com/document/api/266/8586",
-		},
-		{
 			Name:        "CreateClass",
-			Usage:       "CreateClass",
+			Usage:       "Create VOD classification.",
 			Action:      VodCreateClass,
-			Description: "Referer: https://cloud.tencent.com/document/api/266/7812",
-		},
-		{
-			Name:        "DescribeAllClass",
-			Usage:       "DescribeAllClass",
-			Action:      VodDescribeAllClass,
-			Description: "Referer: https://cloud.tencent.com/document/api/266/7813",
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7812",
 		},
 		{
 			Name:        "DescribeClass",
-			Usage:       "DescribeClass",
+			Usage:       "Describe VOD classification.",
 			Action:      VodDescribeClass,
-			Description: "Referer: https://cloud.tencent.com/document/api/266/7814",
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7814",
+		},
+		{
+			Name:        "DescribeAllClass",
+			Usage:       "Describe all VOD classification.",
+			Action:      VodDescribeAllClass,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7813",
 		},
 		{
 			Name:        "ModifyClass",
-			Usage:       "ModifyClass",
+			Usage:       "Modify VOD classification.",
 			Action:      VodModifyClass,
-			Description: "Referer: https://cloud.tencent.com/document/api/266/7815",
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7815",
+		},
+		{
+			Name:        "DeleteClass",
+			Usage:       "Delete VOD classification.",
+			Action:      VodDeleteClass,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7816",
+		},
+		{
+			Name:        "DescribeVodPlayInfo",
+			Usage:       "Returns videofile information.",
+			Action:      VodDescribeVodPlayInfo,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7825",
+		},
+		{
+			Name:        "DescribeRecordPlayInfo",
+			Usage:       "Returns information.",
+			Action:      VodDescribeRecordPlayInfo,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/8227",
+		},
+		{
+			Name:        "GetVideoInfo",
+			Usage:       "Returns videofile information.",
+			Action:      VodGetVideoInfo,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/8586",
+		},
+		{
+			Name:        "QueryWatermarkTemplateList",
+			Usage:       "Returns list of watermark templates.",
+			Action:      VodQueryWatermarkTemplateList,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/11608",
+		},
+		{
+			Name:        "QueryWatermarkTemplate",
+			Usage:       "Returns information on watermark template",
+			Action:      VodQueryWatermarkTemplate,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/11606",
+		},
+		{
+			Name:        "CreateWatermarkTemplate",
+			Usage:       "Creates a new watermark template",
+			Action:      VodCreateWatermarkTemplate,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/11599",
+		},
+		{
+			Name:        "UpdateWatermarkTemplate",
+			Usage:       "Updates a watermark template",
+			Action:      VodUpdateWatermarkTemplate,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/11605",
+		},
+		{
+			Name:        "DeleteWatermarkTemplate",
+			Usage:       "Deletes a watermark template",
+			Action:      VodDeleteWatermarkTemplate,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/11604",
+		},
+		{
+			Name:        "QueryTranscodeTemplateList",
+			Usage:       "Queries list of transcode templates",
+			Action:      VodQueryTranscodeTemplateList,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/9913",
+		},
+		{
+			Name:        "CreateTranscodeTemplate",
+			Usage:       "Creates a new template",
+			Action:      VodCreateTranscodeTemplate,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/9910",
+		},
+		{
+			Name:        "QueryTranscodeTemplate",
+			Usage:       "Querys a particular transcode template",
+			Action:      VodQueryTranscodeTemplate,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/9912",
+		},
+		{
+			Name:        "DeleteTranscodeTemplate",
+			Usage:       "Deletes a transcode template",
+			Action:      VodDeleteTranscodeTemplate,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/9914",
+		},
+		{
+			Name:        "CreateVodTags",
+			Usage:       "Creates a tag on a videofile",
+			Action:      VodCreateVodTags,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7826",
+		},
+		{
+			Name:        "DeleteVodTags",
+			Usage:       "Deletes a tag on a videofile",
+			Action:      VodDeleteVodTags,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7827",
+		},
+		{
+			Name:        "ModifyVodInfo",
+			Usage:       "Deletes a tag on a videofile",
+			Action:      VodModifyVodInfo,
+			Description: "referrer: https://cloud.tencent.com/document/api/266/7828",
 		},
 	}
 )
@@ -66,57 +144,8 @@ func VodDoAction(c *cli.Context) error {
 	fmt.Println(string(resp))
 	return nil
 }
-
-func VodApplyUpload(c *cli.Context) error {
-	resp, err := vod.ApplyUpload(c.Args()...)
-	if err != nil {
-		return err
-	}
-	r, err := resp.String(formatOut)
-	if err != nil {
-		return err
-	}
-	fmt.Println(r)
-	return nil
-}
-func VodDescribeVodPlayInfo(c *cli.Context) error {
-	resp, err := vod.DescribeVodPlayInfo(c.Args()...)
-	if err != nil {
-		return err
-	}
-	r, err := resp.String(formatOut)
-	if err != nil {
-		return err
-	}
-	fmt.Println(r)
-	return nil
-}
-func VodGetVideoInfo(c *cli.Context) error {
-	resp, err := vod.GetVideoInfo(c.Args()...)
-	if err != nil {
-		return err
-	}
-	r, err := resp.String(formatOut)
-	if err != nil {
-		return err
-	}
-	fmt.Println(r)
-	return nil
-}
 func VodCreateClass(c *cli.Context) error {
 	resp, err := vod.CreateClass(c.Args()...)
-	if err != nil {
-		return err
-	}
-	r, err := resp.String(formatOut)
-	if err != nil {
-		return err
-	}
-	fmt.Println(r)
-	return nil
-}
-func VodDescribeAllClass(c *cli.Context) error {
-	resp, err := vod.DescribeAllClass(c.Args()...)
 	if err != nil {
 		return err
 	}
@@ -139,8 +168,212 @@ func VodDescribeClass(c *cli.Context) error {
 	fmt.Println(r)
 	return nil
 }
+func VodDescribeAllClass(c *cli.Context) error {
+	resp, err := vod.DescribeAllClass(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
 func VodModifyClass(c *cli.Context) error {
 	resp, err := vod.ModifyClass(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodDeleteClass(c *cli.Context) error {
+	resp, err := vod.DeleteClass(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodDescribeVodPlayInfo(c *cli.Context) error {
+	resp, err := vod.DescribeVodPlayInfo(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodDescribeRecordPlayInfo(c *cli.Context) error {
+	resp, err := vod.DescribeRecordPlayInfo(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodGetVideoInfo(c *cli.Context) error {
+	resp, err := vod.GetVideoInfo(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodQueryWatermarkTemplateList(c *cli.Context) error {
+	resp, err := vod.QueryWatermarkTemplateList(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodQueryWatermarkTemplate(c *cli.Context) error {
+	resp, err := vod.QueryWatermarkTemplate(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodCreateWatermarkTemplate(c *cli.Context) error {
+	resp, err := vod.CreateWatermarkTemplate(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodUpdateWatermarkTemplate(c *cli.Context) error {
+	resp, err := vod.UpdateWatermarkTemplate(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodDeleteWatermarkTemplate(c *cli.Context) error {
+	resp, err := vod.DeleteWatermarkTemplate(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodQueryTranscodeTemplateList(c *cli.Context) error {
+	resp, err := vod.QueryTranscodeTemplateList(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodCreateTranscodeTemplate(c *cli.Context) error {
+	resp, err := vod.CreateTranscodeTemplate(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodQueryTranscodeTemplate(c *cli.Context) error {
+	resp, err := vod.QueryTranscodeTemplate(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodDeleteTranscodeTemplate(c *cli.Context) error {
+	resp, err := vod.DeleteTranscodeTemplate(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodCreateVodTags(c *cli.Context) error {
+	resp, err := vod.CreateVodTags(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodDeleteVodTags(c *cli.Context) error {
+	resp, err := vod.DeleteVodTags(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func VodModifyVodInfo(c *cli.Context) error {
+	resp, err := vod.ModifyVodInfo(c.Args()...)
 	if err != nil {
 		return err
 	}
--- vod/gen.go
+++ vod/gen.go
@@ -14,10 +14,30 @@ func (p *Pkg) DoAction(action string, query ...string) ([]byte, error) {
 }
 
 func main() {
-	fileName := "fileName=test0"
-	fileId := "fileId=4564972818475166253"
-	className := "className=tcpicliclass"
-	classNameMod := className + "new"
+	region := "Region=bj"                                                             // common input param
+	className := "className=apitest"                                                  // CreateClass etc
+	classNameMod := "className=apitestmod"                                            // ModifyClass
+	fileName := "fileName=apitest"                                                    // DescribeVodPlayInfo // need to statically bootstrap because cannot upload files until COS API is ironed out
+	watermarkName := "name=apitest"                                                   // CreateWatermarkTemplate
+	watermarkUrl := "url=tb1.bdstatic.com/tb/cms/ngmis/images/file_1512126542603.jpg" // CreateWatermarkTemplate
+	width := "width=100px"                                                            // CreateWatermarkTemplate
+	watermarkType := "type=image"                                                     // CreatewatermarkTemplate
+	widthMod := "width=110px"                                                         // UpdateWatermarkTemplate
+	transcodeName := "name=apitest"                                                   // QueryTranscodeTemplateList
+	transcodeContainer := "container=mp4"                                             // CreateTranscodeTemplate
+	transcodeComment := "comment=apitest"                                             // CreateTranscodeTemplate
+	transcodeVideoCodec := "video.codec=libx264"                                      // CreateTranscodeTemplate
+	transcodeFps := "video.fps=45"                                                    // CreateTranscodeTemplate
+	transcodeWidth := "video.width=1280"                                              // CreateTranscodeTemplate
+	transcodeHeight := "video.height=720"                                             // CreateTranscodeTemplate
+	transcodeVideoBitrate := "video.bitrate=512"                                      // CreateTranscodeTemplate
+	transcodeAudioCodec := "audio.codec=libfdk_aac"                                   // CreateTranscodeTemplate
+	transcodeAudioBitrate := "audio.bitrate=256"                                      // CreateTranscodeTemplate
+	transcodeSoundSystem := "audio.soundSystem=2"                                     // CreateTranscodeTemplate
+	transcodeAudioSampleRate := "audio.sampleRate=44100"                              // CreateTranscodeTemplate
+	transcodeCommentMod := "comment=apitestmod"                                       // UpdateTranscodeTemplate
+	tags := "tags.1=apitest"                                                          // CreateVodTags // 1 index -_-
+	vodDescriptionMod := "fileIntro=apitestmod"                                       // ModifyVodInfo // fileIntro... *sigh*
 
 	gen := &autogen.Gen{
 		DocRoot: "https://cloud.tencent.com/document/api/",
@@ -25,36 +45,178 @@ func main() {
 			// -- hold on upload stuff because it has COS API dependency --
 			// "ApplyUpload",
 			// "CommitUpload",
+			// "MultiPullVodFile",
+			// "ApplyUploadWatermark",
 
+			// "RunProcedure",
+			// "ProcessFile",
+			// "ConvertVodFile",
+			// "ClipVideo",
+			// "ConcatVideo",
+			// "CreateSnapshotByTimeOffset",
+			// "CreateImageSprite",
 			"DescribeVodPlayInfo",
+			`SET fileId=tcpicli -f "{{range .FileSet}}{{.FileID}}{{end}}" vod DescribeVodPlayInfo ` + region + " " + fileName,
+			`SET vid=echo $fileId`, // stupid -- two variables, one value ;)
 			"GetVideoInfo",
-
-			// "CreateClass",
-			// "DescribeAllClass",
-			// "DescribeClass",
-			// `SET classId=tcpicli -f "{{range .Data}}{{.Info.ID}}{{end}}" vod DescribeAllClass ` + region + " " + className,
-			// "ModifyClass",
-			// "DeleteClass", // doesn't work for some reason. classId=$classId is giving error 1000; invalid param... is API error, not tcpicli bug
+			"DescribeRecordPlayInfo",
+			"CreateVodTags",
+			"DeleteVodTags",
+			// "DeleteVodFile",
+			// "ModifyVodInfo",
+			"CreateClass",
+			"DescribeAllClass",
+			"DescribeClass",
+			// inspect for vod sucks because API doesn't allow you to filter by className or name or anything descriptive; only barfs all classes at you. there is an immortal default class, therefore, to filter I need to use bash...
+			`SET classId=tcpicli vod DescribeClass ` + region + " | grep -B1 $(echo " + className + " | cut -d '=' -f2)" + ` | grep -v name | awk '{ print $2 }' | tr -d "\","`,
+			"ModifyClass",
+			"DeleteClass",
+			// "PullEvent",
+			// "ConfirmEvent",
+			// "GetTaskList",
+			// "GetTaskInfo",
+			// "RedoTask",
+			"CreateTranscodeTemplate",
+			"QueryTranscodeTemplateList",
+			`SET transcodeDefinition=tcpicli -f "{{range .Data}}{{.Definition}}{{end}}" vod QueryTranscodeTemplateList`,
+			"QueryTranscodeTemplate",
+			"UpdateTranscodeTemplate",
+			"DeleteTranscodeTemplate",
+			"CreateWatermarkTemplate", // can create from http :D
+			"QueryWatermarkTemplateList",
+			`SET watermarkDefinition=tcpicli -f "{{.Zero.Definition}}" vod QueryWatermarkTemplateList ` + watermarkName,
+			"QueryWatermarkTemplate",
+			"UpdateWatermarkTemplate",
+			"DeleteWatermarkTemplate",
+			"DescribeDrmDataKey",
+			// "DescribeVodStorage",
+			// "GetPlayStatLogList",
 		},
 		FuncMap: map[string][]string{
+			// -- hold on upload stuff because it has COS API dependency --
+			// "ApplyUpload":                []string{"266/9756"},
+			// "CommitUpload":               []string{"266/9757"},
+			// "MultiPullVodFile":           []string{"266/7817"},
+			// "ApplyUploadWatermark":       []string{"266/11607"},
+
+			"RunProcedure":               []string{"266/11030"},
+			"ProcessFile":                []string{"266/9642"},
+			"ConvertVodFile":             []string{"266/7822"},
+			"ClipVideo":                  []string{"266/10156"},
+			"ConcatVideo":                []string{"266/7821"},
+			"CreateSnapshotByTimeOffset": []string{"266/8102"},
+			"CreateImageSprite":          []string{"266/8101"},
+			"GetVideoInfo": []string{"266/8586",
+				region,
+				"fileId=$fileId",
+			},
 			"DescribeVodPlayInfo": []string{"266/7825",
+				region,
 				fileName,
 			},
-			"GetVideoInfo": []string{"266/8586",
-				fileId,
+			"DescribeRecordPlayInfo": []string{"266/8227",
+				region,
+				"vid=$vid",
+			},
+			"CreateVodTags": []string{"266/7826",
+				region,
+				tags,
+				"fileId=$fileId",
+			},
+			"DeleteVodTags": []string{"266/7827",
+				region,
+				tags,
+				"fileId=$fileId",
+			},
+			"DeleteVodFile": []string{"266/7838"},
+			"ModifyVodInfo": []string{"266/7828",
+				region,
+				vodDescriptionMod,
+				"fileId=$fileId",
 			},
 			"CreateClass": []string{"266/7812",
+				region,
 				className,
 			},
+			"DescribeAllClass": []string{"266/7813",
+				region,
+			},
+			"DescribeClass": []string{"266/7814",
+				region,
+			},
 			"ModifyClass": []string{"266/7815",
+				region,
 				classNameMod,
 				"classId=$classId",
 			},
-			"DescribeAllClass": []string{"266/7813"},
-			"DescribeClass":    []string{"266/7814"},
 			"DeleteClass": []string{"266/7816",
+				region,
 				"classId=$classId",
 			},
+			"PullEvent":    []string{"266/7818"},
+			"ConfirmEvent": []string{"266/7819"},
+			"GetTaskList":  []string{"266/11722"},
+			"GetTaskInfo":  []string{"266/11724"},
+			"RedoTask":     []string{"266/11725"},
+			"CreateTranscodeTemplate": []string{"266/9910",
+				region,
+				transcodeName,
+				transcodeContainer,
+				transcodeComment,
+				transcodeVideoCodec,
+				transcodeFps,
+				transcodeWidth,
+				transcodeHeight,
+				transcodeVideoBitrate,
+				transcodeAudioCodec,
+				transcodeAudioBitrate,
+				transcodeSoundSystem,
+				transcodeAudioSampleRate,
+			},
+			"QueryTranscodeTemplateList": []string{"266/9913",
+				region,
+				transcodeName,
+			},
+			"QueryTranscodeTemplate": []string{"266/9912",
+				region,
+				"definition=$transcodeDefinition",
+			},
+			"UpdateTranscodeTemplate": []string{"266/9911",
+				region,
+				transcodeCommentMod,
+				"definition=$transcodeDefinition",
+			},
+			"DeleteTranscodeTemplate": []string{"266/9914",
+				region,
+				"definition=$transcodeDefinition",
+			},
+			"CreateWatermarkTemplate": []string{"266/11599",
+				region,
+				watermarkName,
+				watermarkUrl,
+				width,
+				watermarkType,
+			},
+			"QueryWatermarkTemplateList": []string{"266/11608",
+				region,
+				watermarkName,
+			},
+			"QueryWatermarkTemplate": []string{"266/11606",
+				region,
+				"definition=$watermarkDefinition",
+			},
+			"UpdateWatermarkTemplate": []string{"266/11605",
+				region,
+				widthMod,
+				"definition=$watermarkDefinition",
+			},
+			"DeleteWatermarkTemplate": []string{"266/11604",
+				region,
+				"definition=$watermarkDefinition",
+			},
+			"DescribeDrmDataKey": []string{"266/9643"},
+			"DescribeVodStorage": []string{"266/10012"},
+			"GetPlayStatLogList": []string{"266/12624"},
 		},
 		PkgName: "vod",
 		Pkg:     new(Pkg),
