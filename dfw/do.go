2e5fdb112ad7c8ed76ce1e7b2213596598e50067
James Oppel <jamesoppel@tencent.com>
2017-12-08 14:25:16 -0800

    finished dfw

--- dfw/do.go
+++ dfw/do.go
@@ -7,5 +7,10 @@ import (
 var requesturl string = core.Endpoint["dfw"]
 
 func DoAction(action string, options ...string) ([]byte, error) {
+	region, ok := core.HasRegion(options...)
+	if !ok {
+		region = core.DefaultRegion()
+		options = append(options, "Region="+region)
+	}
 	return core.DoAction("dfw", action, options...)
 }
