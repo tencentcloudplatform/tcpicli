60d7d464b3215bf6bc05aa3ab127161bc88d7ff1
kiyor <cai@kiyor.com>
2017-12-12 23:38:34 +0000

    add client useragent

---
+++ core/version.go
@@ -0,0 +1,21 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : version.go
+
+* Purpose :
+
+* Creation Date : 12-12-2017
+
+* Last Modified : Tue 12 Dec 2017 10:48:55 PM UTC
+
+* Created By : Kiyor
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+package core
+
+import ()
+
+const (
+	VERSION = "1.0.0"
+)
