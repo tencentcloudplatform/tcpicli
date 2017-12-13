617bea8956c887fd374f70b64353663df13737d8
James Oppel <jamesoppel@tencent.com>
2017-12-12 16:15:39 -0800

    implemented default region to ccs, eip, lb and vpc before merge to master

--- ccs/do.go
+++ ccs/do.go
@@ -15,6 +15,11 @@ func NewClient() *CcsClient {
 }
 
 func DoAction(action string, options ...string) ([]byte, error) {
+	region, ok := core.HasRegion(options...)
+	if !ok {
+		region = core.DefaultRegion()
+		options = append(options, "Region="+region)
+	}
 	return DefaultClient.Client.DoAction("ccs", action, options...)
 }
 
--- vpc/do.go
+++ vpc/do.go
@@ -15,6 +15,12 @@ func NewClient() *VpcClient {
 }
 
 func DoAction(action string, options ...string) ([]byte, error) {
+	region, ok := core.HasRegion(options...)
+	if !ok {
+		region = core.DefaultRegion()
+		options = append(options, "Region="+region)
+	}
+
 	return DefaultClient.Client.DoAction("vpc", action, options...)
 }
 
