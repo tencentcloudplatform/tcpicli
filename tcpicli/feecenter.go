063c0fb9dd1ce042854ec78d9b31bdc60286f13b
james <jamesoppel@tencent.com>
2018-02-13 10:07:05 -0800

    added feecenter actions as requested

--- tcpicli/feecenter.go
+++ tcpicli/feecenter.go
@@ -13,6 +13,18 @@ var (
 			Usage:  "do action",
 			Action: FeecenterDoAction,
 		},
+		{
+			Name:        "DescribeBills",
+			Usage:       "Describes bills on account",
+			Action:      FeecenterDescribeBills,
+			Description: "referrer: https://cloud.tencent.com/document/api/378/10770",
+		},
+		{
+			Name:        "DescribeResourceBills",
+			Usage:       "Describes resource bills on account",
+			Action:      FeecenterDescribeResourceBills,
+			Description: "referrer: https://cloud.tencent.com/document/api/378/10772",
+		},
 	}
 )
 
@@ -22,5 +34,29 @@ func FeecenterDoAction(c *cli.Context) error {
 		return err
 	}
 	fmt.Println(string(resp))
+	return nil
+}
+func FeecenterDescribeBills(c *cli.Context) error {
+	resp, err := feecenter.DescribeBills(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+func FeecenterDescribeResourceBills(c *cli.Context) error {
+	resp, err := feecenter.DescribeResourceBills(c.Args()...)
+	if err != nil {
+		return err
+	}
+	r, err := resp.String(formatOut)
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
 	return nil
 }
